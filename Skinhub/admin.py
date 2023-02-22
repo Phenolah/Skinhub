7d5392eff87c0c393da9abd722d34d2df997d744
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-02-21 23:53:27 -0800

    Update admin.py

--- Skinhub/admin.py
+++ Skinhub/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'ordered']
+
+    
 admin.site.register(Item)
 admin.site.register(OrderItem)
 admin.site.register(Order)
