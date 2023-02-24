c2b31af16daf9fccd932a69b1d3ac65621f9eda3
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-02-24 04:35:24 -0800

    Update admin.py

--- Skinhub/admin.py
+++ Skinhub/admin.py
@@ -1,13 +1,42 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ['user', 'ordered']
+def make_refund_accepted(modeladmin, request, queryset):
+    queryset.update(refund_requested=False, refund_granted=True)
+make_refund_accepted.short_description ='Update orders to refund granted'
 
-    
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer',
+                    'ordered',
+                    'being_delivered',
+                    'received',
+                    'refund_requested',
+                    'refund_granted',
+                    'discount_coupon',
+                    'payment',
+                    'ref_code']
+    list_display_links = [
+        'customer',
+        'payment',
+        'discount_coupon'
+    ]
+    list_filter = ['ordered',
+                   'being_delivered',
+                   'received',
+                   'refund_requested',
+                   'refund_granted']
+    search_fields = [
+        'customer',
+        'ref_code'
+    ]
+    actions = [make_refund_accepted]
+
+
+# Register your models here.
 admin.site.register(Item)
 admin.site.register(OrderItem)
-admin.site.register(Order)
+#admin.site.register(Order)
 admin.site.register(Payment)
 admin.site.register(DiscountCode)
+admin.site.register(Order,OrderAdmin)
