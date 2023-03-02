abddfa5a2ab00ee5ac72f1c892b69a2b0e67f958
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-03-02 05:09:49 -0800

    Update urls.py

--- Skinhub/urls.py
+++ Skinhub/urls.py
@@ -13,15 +13,16 @@ urlpatterns = [
     path('about/', views.about, name="about"),
     path("add-to-cart/<slug>/", views.add_to_cart, name="add-to-cart"),
     path("remove-from-cart/<slug>/", views.remove_from_cart , name="remove-from-cart"),
-    path("accounts/login/",views.accounts, name="accounts"),
+    path("accounts/",views.accounts, name="accounts"),
     path('details/<slug>/', views.details, name="details"),
     path('Order_Summary/', views.OrderSummary.as_view(), name="order_summary"),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path("remove-single-item-from-cart/<slug>/", views.remove_single_item_from_cart , name="remove-single-item-from-cart"),
     path('checkout/', views.CheckoutView.as_view(), name="checkout"),
     path('payment/<payment_option>', views.PaymentView.as_view(), name='payment'),
     path('coupon/<code>', views.add_coupon, name="coupon"),
     path('refund/', views.RequestRefundView.as_view(), name='refund')
+    path('login/', views.login, name='login'),
+    path('logout',LogoutView.as_view(next_page='login', template_name="registration/logout.html") , name='logout'),
 
 
 
