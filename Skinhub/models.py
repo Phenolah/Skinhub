53318488e3ab40e18dde7892e09352fbe177b3a5
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-03-03 12:24:17 -0800

    Update models.py

--- Skinhub/models.py
+++ Skinhub/models.py
@@ -4,6 +4,16 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Customer(models.Model):
+     name = models.CharField(max_length=200, null=True)
+     phone = models.CharField(max_length=200, null=True)
+     email = models.CharField(max_length=200, null=True)
+     date_created = models.DateTimeField(auto_now_add=TRue, null=True)
+     
+     def __str__(self):
+     return self.name
+     
+
 class Item(models.Model):
     SIZE= (
         ("Small", "Small"),
