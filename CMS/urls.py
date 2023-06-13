04f8ee54ada8c88c668347313d8c39367d2a2521
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-06-13 23:25:43 +0300

    Update urls.py

--- CMS/urls.py
+++ CMS/urls.py
@@ -1,7 +1,7 @@
-"""CMS URL Configuration
+"""moviegenerator URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,17 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('api-auth/', include('rest_framework.urls')),
+    path('/',include('moviefinder.urls')),
 
-    path('', include('Skinhub.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
 urlpatterns += (static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) )
